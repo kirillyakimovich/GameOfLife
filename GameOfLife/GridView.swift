18a0377b9d982f144659be6fbe8ab30c6ec3a90a
Kirill <kirill.yakimovich@gmail.com>
2017-02-03 15:14:26 +0300

    [UPD] drawing logic revisited

--- GameOfLife/GridView.swift
+++ GameOfLife/GridView.swift
@@ -16,8 +16,10 @@ class GridView: UIView {
     lazy var touchRecognizer: UITapGestureRecognizer = {
         UITapGestureRecognizer(target: self, action: #selector(self.touchAction(_:)) )
     } ()
+    fileprivate var activeRect: CGRect = CGRect.zero
     
     var gridModel: GridModel?
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -37,60 +39,80 @@ class GridView: UIView {
         aCoder.encode(gridModel, forKey: "model")
     }
     
-    var xStep: CGFloat {
-        return bounds.width / CGFloat(gridModel!.width)
+    func stepLength(spread: CGFloat, dencity: Int) -> CGFloat{
+        return spread / CGFloat(dencity)
     }
     
-    var yStep: CGFloat {
-        return bounds.height / CGFloat(gridModel!.height)
+    func drawGrid(for model: GridModel, at rect: CGRect, in context: CGContext) {
+        context.setStrokeColor(gridColor.cgColor)
+        context.setLineWidth(1)
+
+        let xOrigin = rect.minX
+        let yOrigin = rect.minY
+        let xStep = stepLength(spread: rect.size.width, dencity: model.width)
+        for lineNumber in 0...model.width {
+            let x = xOrigin + CGFloat(lineNumber) * xStep
+            let top = CGPoint(x: x, y: yOrigin)
+            context.move(to: top)
+            let bottom = CGPoint(x: x, y:  rect.maxY)
+            context.addLine(to: bottom)
+            context.strokePath()
+        }
+        
+        let yStep = stepLength(spread: rect.size.height, dencity: model.height)
+        for lineNumber in 0...model.height {
+            let y = yOrigin + CGFloat(lineNumber) * yStep
+            let left = CGPoint(x: xOrigin, y: y)
+            context.move(to: left)
+            let right = CGPoint(x:rect.maxX, y: y)
+            context.addLine(to: right)
+            context.strokePath()
+        }
+    }
+    
+    func draw(model: GridModel, at rect: CGRect, in context: CGContext) {
+        let xStep = stepLength(spread: rect.size.width, dencity: model.width)
+        let yStep = stepLength(spread: rect.size.height, dencity: model.height)
+        for x in 0..<model.width {
+            for y in 0..<model.height {
+                let cellRect = CGRect(x: rect.minX + CGFloat(x) * xStep,
+                                      y: rect.minY + CGFloat(y) * yStep,
+                                      width: xStep,
+                                      height: yStep)
+                if gridModel!.isAliveAt(x: x, y: y) {
+                    context.setFillColor(aliveColor.cgColor)
+                } else {
+                    context.setFillColor(deadColor.cgColor)
+                }
+                context.fill(cellRect)
+            }
+        }
     }
     
     override func draw(_ rect: CGRect) {
-        let width = gridModel!.width
-        let height = gridModel!.height
-        
-        let context = UIGraphicsGetCurrentContext()
-        
-        context?.setLineWidth(1)
-    
-        for x in 0..<width {
-            for y in 0..<height {
-                let cellRect = CGRect(x: CGFloat(x) * xStep, y: CGFloat(y) * yStep, width: xStep, height: yStep)
-                if gridModel!.isAliveAt(x: x, y: y) {
-                    context?.setFillColor(aliveColor.cgColor)
-                } else {
-                    context?.setFillColor(deadColor.cgColor)
-                }
-                context?.fill(cellRect)
-            }
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
         }
         
-        context?.setStrokeColor(gridColor.cgColor)
-        for lineNumber in 0...width {
-            let x = CGFloat(lineNumber) * xStep
-            let top = CGPoint(x: x, y: 0)
-            context?.move(to: top)
-            let bottom = CGPoint(x: x, y: rect.height)
-            context?.addLine(to: bottom)
-            context?.strokePath()
-        }
+        let rect = rect.insetBy(dx: 50, dy: 50)
         
-        for lineNumber in 0...height {
-            let y = CGFloat(lineNumber) * yStep
-            let left = CGPoint(x: 0, y: y)
-            context?.move(to: left)
-            let right = CGPoint(x:rect.width, y: y)
-            context?.addLine(to: right)
-            context?.strokePath()
-        }
+        activeRect = rect
+        draw(model: gridModel!, at: rect, in: context)
+        drawGrid(for: gridModel!, at: rect, in: context)
     }
 }
 
 extension GridView {
     func touchAction(_ sender:UITapGestureRecognizer) {
         let location = sender.location(in: self)
-        let xIndex = (location.x / (xStep)).rounded(.towardZero)
-        let yIndex = (location.y / (yStep)).rounded(.towardZero)
+        guard activeRect.contains(location) else {
+            return
+        }
+        let xStep = stepLength(spread: activeRect.size.width, dencity: gridModel!.width)
+        let yStep = stepLength(spread: activeRect.size.height, dencity: gridModel!.height)
+
+        let xIndex = ((location.x - activeRect.minX) / (xStep)).rounded(.towardZero)
+        let yIndex = ((location.y - activeRect.minY) / (yStep)).rounded(.towardZero)
         gridModel!.toggleAt(x: Int(xIndex), y: Int(yIndex))
         setNeedsDisplay()
     }
