fe72c0602f4f6e027ce8565114395235e611f347
Kirill Yakimovich <kirill.yakimovich@gmail.com>
2017-02-05 16:30:19 +0300

    [ADD] missing GridTests is back

---
+++ GameOfLifeTests/GridTests.swift
@@ -0,0 +1,62 @@
+//
+//  GridTests.swift
+//  GameOfLife
+//
+//  Created by Kirill Yakimovich on 2/3/17.
+//  Copyright © 2017 Kirill Yakimovich. All rights reserved.
+//
+
+import XCTest
+@testable import GameOfLife
+
+class GridTests: XCTestCase {
+    var grid = Grid([[1, 2], [3, 4], [5, 6]])
+    
+    func testSubscriptForFirstElement() {
+        XCTAssertEqual(grid[0, 0], 1)
+    }
+    
+    func testSubscriptForLastElement() {
+        XCTAssertEqual(grid[1, 2], 6)
+    }
+    
+    func testAnotherSubscript() {
+        grid[0, 1] = -10
+        XCTAssertEqual(grid, Grid([[1, -10], [3, 4], [5, 6]]))
+    }
+    
+    func testGetRowSubscript() {
+        XCTAssertEqual(grid[row: 2], [5, 6])
+    }
+    
+    func testSetRowSubscript() {
+        grid[row: 2] = [-7, -8]
+        XCTAssertEqual(grid, Grid([[1, 2], [3, 4], [-7, -8]]))
+    }
+    
+    func testGetColumnSubscript() {
+        XCTAssertEqual(grid[column: 0], [1, 3, 5])
+    }
+    
+    func testSetColumnSubscript() {
+        grid[column: 0] = [-7, -8, -9]
+        XCTAssertEqual(grid, Grid([[-7, 2], [-8, 4], [-9, 6]]))
+    }
+    
+    func testBackwardCycledSubscript() {
+        XCTAssertEqual(grid[cycledRow: -1, cycledColumn: -1], 6)
+    }
+    
+    func testForwardCycledSubscript() {
+        XCTAssertEqual(grid[cycledRow: 100, cycledColumn: 100], 1)
+    }
+
+    func testContainsReturnsTrueForPresetnElement() {
+        XCTAssertTrue(grid.contains(3))
+    }
+    
+    func testContainsReturnsFalseForAbsentElement() {
+        XCTAssertFalse(grid.contains(300))
+    }
+
+}
