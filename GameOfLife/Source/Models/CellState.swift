3e24381b07638696bf3a3e8751a48b4f0229b80e
Kirill Yakimovich <kirill.yakimovich@gmail.com>
2017-02-05 16:48:14 +0300

    [UPD] CellState now could be initialized with rle tag, valid or not - doesn't matter

--- GameOfLife/Source/Models/CellState.swift
+++ GameOfLife/Source/Models/CellState.swift
@@ -9,30 +9,42 @@
 import Foundation
 
 enum CellState: Int {
-    case alive
     case dead
+    case alive
     
     func switched() -> CellState {
         switch self {
-        case .alive:
-            return .dead
         case .dead:
             return .alive
+        case .alive:
+            return .dead
         }
     }
     
     func shouldSwitch(aliveNeighbours: Int) -> Bool {
         switch self {
-        case .alive:
-            if aliveNeighbours < 2 || aliveNeighbours > 3 {
-                return true
-            }
         case .dead:
             if aliveNeighbours == 3 {
+                return true
+            }
+        case .alive:
+            if aliveNeighbours < 2 || aliveNeighbours > 3 {
                 return true
             }
         }
         
         return false
+    }
+}
+
+// b - dead cell
+// o	alive cell
+extension CellState {
+    init(rleTag: String) {
+        if rleTag == "o" {
+            self = .alive
+        } else {
+            self = .dead
+        }
     }
 }
